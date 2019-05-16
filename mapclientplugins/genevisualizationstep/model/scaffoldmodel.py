690b0123f6cdd6e7293d59545bd2e93f6dd9ce70
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-05-16 18:07:09 +1200

    Separate methods for different graphic object.

--- mapclientplugins/genevisualizationstep/model/scaffoldmodel.py
+++ mapclientplugins/genevisualizationstep/model/scaffoldmodel.py
@@ -24,6 +24,7 @@ class ScaffoldModel(object):
     def _create_graphics(self):
         self._create_line_graphics()
         self._create_surface_graphics()
+        # self._create_node_graphics()
 
     def _create_line_graphics(self):
         lines = self._scene.createGraphicsLines()
@@ -32,6 +33,17 @@ class ScaffoldModel(object):
         black = self._material_module.findMaterialByName('heart_lines')
         lines.setMaterial(black)
         return lines
+
+    def _create_node_graphics(self):
+        nodepoints = self._scene.createGraphicsPoints()
+        nodepoints.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        nodepoints.setCoordinateField(self._coordinates)
+        pointAttr = nodepoints.getGraphicspointattributes()
+        pointAttr.setGlyphShapeType(Glyph.SHAPE_TYPE_SPHERE)
+        pointAttr.setBaseSize([0.05])
+        green = self._material_module.findMaterialByName('green')
+        nodepoints.setMaterial(green)
+        return nodepoints
 
     def _create_scene(self):
         return self._scaffold_region.getScene()
@@ -90,64 +102,8 @@ class ScaffoldModel(object):
     def _setup_scene(self):
         self._scene = self._create_scene()
         self._scene.beginChange()
-        materialmodule = self._context.getMaterialmodule()
-
-        # view the 1-D line elements on the edges of the cube
-        lines = self._scene.createGraphicsLines()
-        lines.setCoordinateField(self._coordinates)
-        lines.setName('display_lines')
-        black = self._material_module.findMaterialByName('heart_lines')
-        lines.setMaterial(black)
-
-        # view coordinate axes at the origin
-        axes = self._scene.createGraphicsPoints()
-        axes.setFieldDomainType(Field.DOMAIN_TYPE_POINT)
-        pointAttr = axes.getGraphicspointattributes()
-        pointAttr.setGlyphShapeType(Glyph.SHAPE_TYPE_AXES_XYZ)
-        pointAttr.setBaseSize([1.2])
-        blue = materialmodule.findMaterialByName('blue')
-        axes.setMaterial(blue)
-
-        # view the nodes at the corners of the cube as green spheres
-        # nodepoints = self._scene.createGraphicsPoints()
-        # nodepoints.setFieldDomainType(Field.DOMAIN_TYPE_NODES)
-        # nodepoints.setCoordinateField(self._coordinates)
-        # pointAttr = nodepoints.getGraphicspointattributes()
-        # pointAttr.setGlyphShapeType(Glyph.SHAPE_TYPE_SPHERE)
-        # pointAttr.setBaseSize([0.05])
-        # green = materialmodule.findMaterialByName('green')
-        # nodepoints.setMaterial(green)
-
-        # view 'isosurfaces' where the magnitude of coordinates equals 1.0
-        # isosurfaces = self._scene.createGraphicsContours()
-        # isosurfaces.setCoordinateField(self._coordinates)
-        # isosurfaces.setIsoscalarField(self._magnitude)
-        # isosurfaces.setListIsovalues(1.0);
-        # gold = materialmodule.findMaterialByName('gold')
-        # isosurfaces.setMaterial(gold)
-        # # use finer tessellation to see the curvature of the isosurfaces
-        # tessellationmodule = self._context.getTessellationmodule()
-        # fineTessellation = tessellationmodule.createTessellation()
-        # fineTessellation.setName('fine') # name it so it can be found by name later
-        # fineTessellation.setManaged(True) # manage its lifetime so it is kept even if not being used
-        # fineTessellation.setMinimumDivisions(8) # divide element edges into 8 line segments
-        # isosurfaces.setTessellation(fineTessellation)
-
-        surface = self._scene.createGraphicsSurfaces()
-        surface.setCoordinateField(self._coordinates)
-        surface.setRenderPolygonMode(Graphics.RENDER_POLYGON_MODE_SHADED)
-        surface_material = self._material_module.findMaterialByName('heart_tissue')
-        surface.setMaterial(surface_material)
-        surface.setName('display_surface')
-        surface.setVisibilityFlag(self.is_display_surface('display_surface'))
-
-        # self._create_graphics()
-
+        self._create_graphics()
         self._scene.endChange()
-
-        # line = self._create_line_graphics()
-        # line.setRenderLineWidth(2.5)
-        # self._surface = self._create_surface_graphics()
 
     def set_display_objects(self, surface_name, show):
         self._set_visibility(surface_name, show)
