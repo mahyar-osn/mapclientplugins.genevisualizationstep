5475c672085d2e2e82a76befde69880be37dafe2
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-05-16 18:06:35 +1200

    Auto-adjust column width + Attempt to add bar and heatmap plotting.

--- mapclientplugins/genevisualizationstep/utils/visualization.py
+++ mapclientplugins/genevisualizationstep/utils/visualization.py
@@ -1,32 +1,51 @@
 import sys
 
+# from PySide import QtGui as PySideQtGui
+
 import pyqtgraph as pg
-# from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 import pandas as pd
 
 
-class Visualization:
+class VisualizationHandler:
 
     def __init__(self, data):
         self._data = data
         # self._app = QtGui.QApplication([])
         self._w = None
 
-    def viewTable(self):
+    def view_table(self):
         self._w = pg.TableWidget()
         self._w.show()
-        self._w.resize(500, 500)
+        self._w.resize(1000, 500)
         self._w.setWindowTitle("Table for the %s RNASeq data" % self._data['data'])
 
         # re-structure the data frame into a numpy array
         darray = np.hstack((np.asarray(self._data['id'], dtype=object).reshape(len(self._data['id']), 1),
                             np.asarray(self._data['gene'], dtype=float)))
-        a = pd.concat((self._data['id'], self._data['gene']), axis=1)
         self._w.setData(np.asarray(darray))
+
+        # Cell ID column with different color
+        for item in range(len(darray)):
+            self._w.item(item, 0).setBackground(QtGui.QColor(100, 100, 150))
+
+        # Setup header with gene IDs
         b = ['Sample Name'] + self._data['gene'].columns.values.tolist()
-        self._w.setHorizontalHeaderLabels(([a for a in b]))
-        # if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        #     QtGui.QApplication.instance().exec_()
+        header_labels = [a for a in b]
+        self._w.setHorizontalHeaderLabels(header_labels)
+        self._w.resizeColumnsToContents()
 
+        print('done')
 
+    def view_bar_plot(self):
+        pass
+
+    def view_heatmap(self, cluster=False):
+
+        if cluster:
+            self._make_cluster()
+        pass
+
+    def _make_cluster(self):
+        pass
